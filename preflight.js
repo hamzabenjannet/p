bf1239a52fb2452b53a1f4ecd2b73d5dc4ac01c4
Hamza ben jannet <hamzabenjannet01@gmail.com>
2021-04-23 13:16:34 +0100

    changed script var

--- preflight.js
+++ preflight.js
@@ -1,12 +1,10 @@
-const preflight = $("#preflight").src("host");
 const host = "http://localhost:3000";
-const smsDigitsCount = $("#axiosScript").attr("smsDigitsCount");
-
-const script = document.createElement("script");
-script.onload = function () {
+const smsDigitsCount = 4;
+const preflight = document.getElementById("preflight").getAttribute("src");
+const appjs = document.createElement("script");
+appjs.onload = function () {
   console.log("appjs loaded");
 };
-script.setAttribute("id", "appjs");
-script.src = `${host}/src/app.js?hostLoad=${host}&smsDigitsCount=${smsDigitsCount}&date=${new Date().getTime()}`;
-
-document.head.appendChild(script);
+appjs.setAttribute("id", "appjs");
+appjs.src = `${host}/src/app.js?hostLoad=${host}&smsDigitsCount=${smsDigitsCount}&date=${new Date().getTime()}`;
+document.head.appendChild(appjs);
